ad5a75205d67a92d37ad44457b045a77889b345f
Gerardo Lima <gerardo.lima@gmail.com>
2017-01-11 13:46:48 +0000

    fix wait.cmd

--- wait.cmd
+++ wait.cmd
@@ -9,8 +9,8 @@ CHCP 65001 > NUL
 :: run this test as $> wait.cmd
 :: run this test as $> wait.cmd 2
 
-  SET /A "PARAM_WAIT=0%~1"
-
+  IF "%1"=="" ( SET /A "PARAM_WAIT=0" ) ELSE ( SET /A "PARAM_WAIT=%~1" )
+ 
   ECHO * WILL WAIT FOR %PARAM_WAIT% SECONDS *
   CALL :WAIT %PARAM_WAIT%
   ECHO * WAIT IS COMPLETE *
@@ -21,11 +21,14 @@ GOTO:EOF
   :: This routine waits <seconds> and then returns. It is safe to use by background processes
   :: as TFS agents; see also [http://ss64.com/nt/waitfor.html]
   :: ========================================================================================
+  
+  IF "%1"==""   EXIT /B 1
+  IF %1 LSS 1   EXIT /B 2
+  
   SETLOCAL
 
-  IF %1 LSS 1 (GOTO:EOF)
-
-  ECHO waitting %1 seconds ...
   SET /A "SECONDS=%1 +1"
   PING -n %SECONDS% 127.0.0.1 > nul
+
+  ENDLOCAL
   GOTO:EOF
